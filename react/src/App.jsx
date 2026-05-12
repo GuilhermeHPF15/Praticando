90b20671fe0f4c896ab69b39a48c3ffbe7dc13de
MorganaAPF <morganaapf@gmail.com>
2026-05-12 22:29:41 +0100

    useState learned

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,22 @@
+import { useState } from "react";
+
 //Função, retorno e exportação. São os 3 elementos essenciais de um componente React.
 function App() {
-  const nome = "Guilherme";
+  const name = "Guilherme";
+
+  //Nome do estado(state), a função que tem permissão de alterá-lo, e seu ponto inicial (0).
+  const [count, setCount] = useState(0); 
+  /* useState é um hook. Hooks são ganchos que buscam funcionalidades, neste caso, este hook busca a 
+  funcionalidade de estados. Todo hook começa com "use". */
+  //Hooks precisam estar no nível mais alto da função, nunca dentro de condicionais, loops ou funções aninhadas.
+
+  let [inputName, setInput] = useState("");
+
+  function increase() {
+    setCount(count + 1);
+  }
+
+  console.log("Funcionou!"); //Toda vez que o estado for atualizado, essa linha será executada também.
 
   return (
     //É obrigatório ter um elemento pai que contenha os outros, pois o return só pode retornar um elemento.
@@ -8,8 +24,16 @@ function App() {
       <h1>Olá, mundo!</h1> 
       <p>Meu primeiro experimento!</p>
       <p>Sem div extra aqui!</p>
-      <h1>Laboratório de {nome}.</h1> {/*Inserindo JavaScript dentro das tags.*/}
+
+      <h1>Laboratório de {name}.</h1> {/*Inserindo JavaScript dentro das tags.*/}
       <p>A soma de 2 + 2 é {2 + 2}.</p>
+
+      <h1>Contador científico</h1>
+      <p>Valor atual: {count}</p>
+      <button onClick={increase}></button>
+      <br />
+      <input type="text" onChange={(event) => setInput(inputName = event.target.value)}/>
+      <h2>Olá, {inputName}</h2>
     </>
   ) 
 }
